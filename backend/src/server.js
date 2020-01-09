4c38f0a93a87855a7a7926878a832bd77cd8c1ba
Gilearde <gileardefernandes@gmail.com>
2020-01-08 23:06:50 -0300

    subindo frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express  = require('express');
 const mongoose  = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 
@@ -11,7 +13,9 @@ mongoose.connect('mongodb+srv://micaelfernandes:micaelfernandes@bdestudos-k8myy.
     useUnifiedTopology :true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use( '/files', express.static(path.resolve(__dirname,'..','uploads')))
 app.use(routes);
 
 
